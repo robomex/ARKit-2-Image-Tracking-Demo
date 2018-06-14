4b792f3f029268e82d7d530d6b2cd44d0db8b17c
Tony Morales <atmorales@gmail.com>
2018-06-14 16:53:32 -0500

    ARKit 2 Image Tracking Demo
    Enables Harry Potter style moving images, living movie posters, video postcards, paper-thin "displays," and lots more AR fun

--- Tracked Images/AppDelegate.swift
+++ Tracked Images/AppDelegate.swift
@@ -7,40 +7,25 @@
 //
 
 import UIKit
+import ARKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        guard ARWorldTrackingConfiguration.isSupported else {
+            fatalError("""
+                ARKit is not available on this device. For apps that require ARKit
+                for core functionality, use the `arkit` key in the key in the
+                `UIRequiredDeviceCapabilities` section of the Info.plist to prevent
+                the app from installing. (If the app can't be installed, this error
+                can't be triggered in a production scenario.)
+                In apps where AR is an additive feature, use `isSupported` to
+                determine whether to show UI for launching AR experiences.
+            """) // For details, see https://developer.apple.com/documentation/arkit
+        }
+        
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
-
--- Tracked Images/ViewController.swift
+++ Tracked Images/ViewController.swift
@@ -13,63 +13,157 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var magicSwitch: UISwitch!
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    
+    // Create video player
+    let isaVideoPlayer: AVPlayer = {
+        //load Isa video from bundle
+        guard let url = Bundle.main.url(forResource: "isa video", withExtension: "mp4", subdirectory: "art.scnassets") else {
+            print("Could not find video file")
+            return AVPlayer()
+        }
+        
+        return AVPlayer(url: url)
+    }()
+    let pragueVideoPlayer: AVPlayer = {
+        //load Prague video from bundle
+        guard let url = Bundle.main.url(forResource: "prague video", withExtension: "mp4", subdirectory: "art.scnassets") else {
+            print("Could not find video file")
+            return AVPlayer()
+        }
+        
+        return AVPlayer(url: url)
+    }()
+    let fightClubVideoPlayer: AVPlayer = {
+        //load Prague video from bundle
+        guard let url = Bundle.main.url(forResource: "fight club video", withExtension: "mov", subdirectory: "art.scnassets") else {
+            print("Could not find video file")
+            return AVPlayer()
+        }
+        
+        return AVPlayer(url: url)
+    }()
+    let homerVideoPlayer: AVPlayer = {
+        //load Prague video from bundle
+        guard let url = Bundle.main.url(forResource: "homer video", withExtension: "mov", subdirectory: "art.scnassets") else {
+            print("Could not find video file")
+            return AVPlayer()
+        }
+        
+        return AVPlayer(url: url)
+    }()
+    
+    
+    /// The view controller that displays the status and "restart experience" UI.
+    lazy var statusViewController: StatusViewController = {
+        return children.lazy.compactMap({ $0 as? StatusViewController }).first!
+    }()
+    
+    /// A serial queue for thread safety when modifying the SceneKit node graph.
+    let updateQueue = DispatchQueue(label: Bundle.main.bundleIdentifier! +
+        ".serialSceneKitQueue")
+    
+    /// Convenience accessor for the session owned by ARSCNView.
+    var session: ARSession {
+        return sceneView.session
+    }
+    
+    // MARK: - View Controller Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
         sceneView.delegate = self
+        sceneView.session.delegate = self
+        magicSwitch.setOn(false, animated: false)
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        // Hook up status view controller callback(s).
+        statusViewController.restartExperienceHandler = { [unowned self] in
+            self.restartExperience()
+        }
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
+        // Prevent the screen from being dimmed to avoid interuppting the AR experience.
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+        // Start the AR experience
+        resetTracking()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        // Pause the view's session
-        sceneView.session.pause()
+        session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+    // MARK: - Session management (Image detection setup)
+    
+    /// Prevents restarting the session while a restart is in progress.
+    var isRestartAvailable = true
+    
+    @IBAction func switchOnMagic(_ sender: Any) {
+        let configuration = ARImageTrackingConfiguration()
+        
+        guard let trackingImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else {
+            print("Could not load images")
+            return
+        }
+        
+        // Setup Configuration
+        configuration.trackingImages = trackingImages
+        configuration.maximumNumberOfTrackedImages = 4
+        session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    
+    
+    /// Creates a new AR configuration to run on the `session`.
+    /// - Tag: ARReferenceImage-Loading
+    func resetTracking() {
+        
+        let configuration = ARImageTrackingConfiguration()
+        session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    
+    // MARK: - Image Tracking Results
+    
+    public func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
-     
+        
+        // Show video overlaid on image
+        if let imageAnchor = anchor as? ARImageAnchor {
+            
+            // Create a plane
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+            if imageAnchor.referenceImage.name == "prague image" {
+                // Set AVPlayer as the plane's texture and play
+                plane.firstMaterial?.diffuse.contents = self.pragueVideoPlayer
+                self.pragueVideoPlayer.play()
+                self.pragueVideoPlayer.volume = 0.4
+            } else if imageAnchor.referenceImage.name == "fight club image" {
+                plane.firstMaterial?.diffuse.contents = self.fightClubVideoPlayer
+                self.fightClubVideoPlayer.play()
+            } else if imageAnchor.referenceImage.name == "homer image" {
+                plane.firstMaterial?.diffuse.contents = self.homerVideoPlayer
+                self.homerVideoPlayer.play()
+            } else {
+                plane.firstMaterial?.diffuse.contents = self.isaVideoPlayer
+                self.isaVideoPlayer.play()
+                self.isaVideoPlayer.isMuted = true
+            }
+            
+            let planeNode = SCNNode(geometry: plane)
+            
+            // Rotate the plane to match the anchor
+            planeNode.eulerAngles.x = -.pi / 2
+            
+            // Add plane node to parent
+            node.addChildNode(planeNode)
+        }
+        
         return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
